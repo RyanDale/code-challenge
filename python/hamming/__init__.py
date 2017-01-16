b5fff2bac2e2575db789939430f3fdf68114ed0c
Ryan Dale <rd01748@georgiasouthern.edu>
2017-01-16 13:18:48 -0500

    Add Python compute function to calculate the Hamming difference

--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,8 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
-    pass
+
+def compute(strand_1='', strand_2=''):
+    if len(strand_1) is not len(strand_2):
+        raise ValueError
+    return sum(1 for (index, nucleotide) in enumerate(strand_1) if nucleotide is not strand_2[index])
