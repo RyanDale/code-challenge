105ab891caed85de01afe6e41038dc2c6152e356
Ryan Dale <rd01748@georgiasouthern.edu>
2017-01-16 13:17:03 -0500

    Add JS compute function to calculate the Hamming difference

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -3,5 +3,17 @@
 // your code meets all of the conditions.
 // You may use ES6 or ES5 to solve.
 
+class Hamming {
+    compute(strand1 = '', strand2 = '') {
+        if (strand1.length != strand2.length) {
+            throw 'DNA strands must be of equal length.';
+        }
+
+        // Creating Arrays instead of using a for loop for a more concise solution
+        strand1 = strand1.split('');
+        strand2 = strand2.split('');
+        return strand1.filter((nucleotide, index) => nucleotide !== strand2[index]).length;
+    }
+}
 
 module.exports = Hamming;
